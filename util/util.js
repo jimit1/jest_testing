b89a5e068e0e932d0b2bad770d00f14e8926c18e
Jimit Mehta <jimitmehta@Jimits-MacBook-Pro.local>
2020-06-01 20:35:11 -0700

    added bunch more functions

--- util/util.js
+++ util/util.js
@@ -10,4 +10,22 @@ const minus = (num1, num2) => {
   return num1 - num2;
 };
 
-module.exports = { add, minus };
+const multiply = (num1, num2) => {
+  if (typeof num1 != "number") throw new Error("1st argument must be a number");
+  if (typeof num2 != "number") throw new Error("2nd argument must be a number");
+  return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+  if (typeof num1 != "number") throw new Error("1st argument must be a number");
+  if (typeof num2 != "number") throw new Error("2nd argument must be a number");
+  return num1 / num2;
+};
+
+const power = (num1, num2) => {
+  if (typeof num1 != "number") throw new Error("1st argument must be a number");
+  if (typeof num2 != "number") throw new Error("2nd argument must be a number");
+  return Math.pow(4, 2);
+};
+
+module.exports = { add, minus, multiply, divide, power };
